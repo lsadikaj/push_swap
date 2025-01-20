c352ec1ab7241c1e0e652364a9ae45f86ffbe1ee
Leotrim Sadikaj <lsadikaj@c2r9s2.42lausanne.ch>
2025-01-20 19:06:35 +0100

    PushSwapLeakFixed

--- srcs/push_swap/push_swap.c
+++ srcs/push_swap/push_swap.c
@@ -6,34 +6,64 @@
 /*   By: lsadikaj <lsadikaj@student.42lausanne.ch>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/15 10:11:51 by lsadikaj          #+#    #+#             */
-/*   Updated: 2025/01/16 18:01:55 by lsadikaj         ###   ########.fr       */
+/*   Updated: 2025/01/20 18:58:18 by lsadikaj         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../inc/push_swap.h"
+
+void	free_split(char **split)
+{
+	int	i;
+
+	i = 0;
+	while (split[i])
+	{
+		free(split[i]);
+		i++;
+	}
+	free(split);
+}
+
+void	handle_sorting(t_stack_node **a, t_stack_node **b)
+{
+	if (stack_len(*a) == 2)
+		sa(a, false);
+	else if (stack_len(*a) == 3)
+		sort_three(a);
+	else
+		sort_stacks(a, b);
+}
+
+int	handle_input(int argc, char ***argv, char ***split_argv)
+{
+	if (argc == 1 || (argc == 2 && !(*argv)[1][0]))
+		return (1);
+	else if (argc == 2)
+	{
+		*split_argv = split((*argv)[1], ' ');
+		*argv = *split_argv;
+	}
+	return (0);
+}
 
 // Handle program initialization, input validation, and sorting logic
 int	main(int argc, char **argv)
 {
 	t_stack_node	*a;
 	t_stack_node	*b;
+	char			**split_argv;
 
 	a = NULL;
 	b = NULL;
-	if (argc == 1 || (argc == 2 && !argv[1][0]))
+	split_argv = NULL;
+	if (handle_input(argc, &argv, &split_argv))
 		return (1);
-	else if (argc == 2)
-		argv = split(argv[1], ' ');
 	init_stack_a(&a, argv + 1);
 	if (!stack_sorted(a))
-	{
-		if (stack_len(a) == 2)
-			sa(&a, false);
-		else if (stack_len(a) == 3)
-			sort_three(&a);
-		else
-			sort_stacks(&a, &b);
-	}
+		handle_sorting(&a, &b);
 	free_stack(&a);
+	if (split_argv)
+		free_split(split_argv);
 	return (0);
 }
